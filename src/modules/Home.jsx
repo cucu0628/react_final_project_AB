66f35ddd5ca3b191e9bdf203b3b7c12508e0f853
cucu0628 <adambence2007@gmail.com>
2026-05-23 12:20:04 +0200

    Home center

--- src/modules/Home.jsx
+++ src/modules/Home.jsx
@@ -1,6 +1,8 @@
-const Home = () =>{
-    return(
-        <p class="h1">Home</p>
+const Home = () => {
+    return (
+        <div className="position-absolute top-50 start-50 translate-middle">
+            <p className="h1">Home</p>
+        </div>
     )
 }
 export default Home;
